fbef4d8bd146d4e83b9f19ab64fba5acb309c035
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-08-05 13:57:01 +0900

    frist commit

---
+++ src/components/button/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Button = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Button;
+
+const Btn = styled.button`
+    background-color: brown;
+    width:200px
+`
+export {Btn};
